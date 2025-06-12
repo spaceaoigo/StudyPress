dcbd3e98a1665e50163c25cdeeb289a958f5854b
spaceaoigo <spaceaoigo@gmail.com>
2025-06-13 00:23:18 +0900

    Update settings.py

--- snsproject-master/config/settings.py
+++ snsproject-master/config/settings.py
@@ -9,7 +9,10 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import os
+from pathlib import Path
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
